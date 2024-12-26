c8729b601dc8f09fee30afc34770e583dca22a80
heru <hrstwn@vanilla.localdomain>
2024-12-26 07:05:57 +0000

    cleaning and fixing

--- includes.container/driver/App/usr/lib/xencelabs/restar.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sudo chmod 777 /tmp/qtsingleapp-Xencel-fb8d-lockfile
--- includes.container/driver/App/usr/lib/xencelabs/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pkill xence
-appname=xencelabs
-dirname=/usr/lib/xencelabs
-LD_LIBRARY_PATH=$dirname/lib
-echo "lib path :$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH
-$dirname/$appname "$@"
